d5f18dac04a5b7d9182db6808e1d1d3318d37c96
Nathan Clack <nathan@vidriotech.com>
2017-05-08 10:21:28 -0400

    add build scripts

---
+++ scripts/build.ps1
@@ -0,0 +1,29 @@
+function msbuild($ver) {
+    $regKey = "HKLM:\software\Microsoft\MSBuild\ToolsVersions\$ver"
+    $regProperty = "MSBuildToolsPath"
+    join-path -path (Get-ItemProperty $regKey).$regProperty -childpath "msbuild.exe"
+}
+
+if (-Not (Test-Path build)){
+    $installdir=join-path (pwd) "install"
+    mkdir build
+    cd build
+    cmake -G "Visual Studio 14 2015 Win64" -DCMAKE_INSTALL_PREFIX=$installdir ..    
+    cd ..
+    $conf_ret_code=$LASTEXITCODE
+    if($conf_ret_code -ne 0){
+    throw "CMake failed with exit code $LASTEXITCODE."
+}
+}
+
+write-host "Building ..."
+cd build
+&(msbuild("14.0")) hog.sln /m /nologo /clp:"Verbosity=minimal;ShowTimestamp;"
+$build_ret_code=$LASTEXITCODE
+cd ..
+
+if($build_ret_code -ne 0){
+    throw "MSBUILD failed with exit code $LASTEXITCODE."
+}
+write-host "...Done"
+
