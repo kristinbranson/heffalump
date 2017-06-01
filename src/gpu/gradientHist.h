5f4a435be7519234032babe3c0e58c203fe48118
Nathan Clack <nclack@gmail.com>
2017-06-01 09:18:04 -0400

    getting ready to test

---
+++ src/gpu/gradientHist.h
@@ -0,0 +1,56 @@
+#pragma once
+#ifndef H_NGC_GRADIENT_HISTOGRAM
+#define H_NGC_GRADIENT_HISTOGRAM
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+    struct gradientHistogramParameters {
+        struct { unsigned w,h; } cell;
+        struct { unsigned w,h,pitch; } image;
+        int nbins;
+    };
+
+    struct gradientHistogram {
+        void *workspace; // opaque
+    };
+
+    /// @param logger [in] Must have lifetime longer than this object.  It can be called during `GradientHistogramDestroy()`.
+    ///                    The logger is used to report errors and debugging information back to the caller.
+    void GradientHistogramInit(struct gradientHistogram* self,
+                               const struct gradientHistogramParameters *param,
+                               void(*logger)(int is_error,const char *file,int line,const char* function,const char *fmt,...));
+
+    void GradientHistogramDestroy(struct gradientHistogram* self);
+
+    /// Computes the gradient histogram given dx and dy.
+    ///
+    /// dx and dy are float images with the same memory layout.
+    /// dx represents the gradient in x
+    /// dy represents the gradient in y
+    /// 
+    /// Both images are sized (w,h).  The index of a pixel at (x,y)
+    /// is x+y*p.  w,h and p should be specified in units of pixels.
+    void GradientHistogram(struct gradientHistogram *self,const float *dx,const float *dy);
+      
+    /// Allocate a buffer capable of receiving the result.
+    /// This buffer can be passed to `GradientHistogramCopyLastResult`.
+    void* GradientHistogramAllocOutput(const struct gradientHistogram *self,void* (*alloc)(size_t nbytes));
+
+    void GradientHistogramCopyLastResult(const struct gradientHistogram *self,void *buf,size_t nbytes);
+
+    /// shape and strides are returned in units of float elements.
+    ///
+    /// The shape is the extent of the returned volume.
+    /// The strides describe how far to step in order to move by 1 along the corresponding dimension.
+    /// Or, more precisely, the index of an item at r=(x,y,z) is dot(r,strides).
+    ///
+    /// The last size is the total number of elements in the volume.
+    void GradientHistogramyOutputShape(const struct gradientHistogram *self,unsigned shape[3],unsigned strides[4]);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/gpu/hog.c
@@ -0,0 +1,108 @@
+#include "../hog.h"
+#include "../conv.h"
+#include "gradientHist.h"
+
+#define LOG(L,...) L(0,__FILE__,__LINE__,__FUNCTION__,__VA_ARGS__)
+#define ERR(L,...) L(1,__FILE__,__LINE__,__FUNCTION__,__VA_ARGS__) 
+#define CHECK(L,e) do{if(!(e)){ERR(L,"Expression evaluated as false\n\t%s\n",#e);goto Error;}}while(0)
+
+struct workspace {
+    struct conv_context dx,dy;
+    struct gradientHistogram gh;
+};
+
+static size_t features_nelem(const struct hog_context *self) {
+    struct workspace* ws=(struct workspace*)self->workspace;
+    unsigned shape[3],strides[4];
+    GradientHistogramyOutputShape(&ws->gh,shape,strides);
+    return strides[3];
+}
+
+static size_t features_nbytes(const struct hog_context *self) {
+    return sizeof(float)*features_nelem(self);
+}
+
+static size_t grad_nbytes(const struct hog_context *self) {
+    return sizeof(float)*self->w*self->h;
+}
+
+static struct workspace* workspace_init(const struct hog_context *self) {
+    const int w=self->w,h=self->h;
+    struct workspace* ws=malloc(sizeof(struct workspace););
+    float k[3]={-1,0,1},*ks[]={k,k};
+    unsigned nkx[]={3,0},nky[]={0,3};
+    ws->dx=conv_init(self->logger,w,h,w,ks,nkx); // FIXME: need the real input pitch here
+    ws->dy=conv_init(self->logger,w,h,w,ks,nky); // FIXME: need the real input pitch here
+    
+    struct gradientHistogramParameters params={
+        .cell={ .w=self->params.cell.w,
+                .h=self->params.cell.h},
+        .image={ .w=self->w,
+                 .h=self->h,
+                 .pitch=self->w}, // FIXME: need the real input pitch here
+        .nbins=self->params.nbins
+    }
+    GradientHistogramInit(&ws->gh,params,self->logger);
+    return ws;
+}
+
+struct hog_context hog_init(
+    void(*logger)(int is_error,const char *file,int line,const char* function,const char *fmt,...),
+    const struct hog_parameters params,
+    int w,int h)
+{
+    struct hog_context self={
+        .logger=logger,
+        .params=params,
+        .w=w,.h=h,
+        .workspace=workspace_init(&self)
+    };
+    return self;
+}
+
+void hog_teardown(struct hog_context *self) {
+    struct workspace* ws=(struct workspace*)self->workspace;
+    conv_teardown(&ws->dx);
+    conv_teardown(&ws->dy);
+    GradientHistogramDestroy(&ws->gh);
+    free(self->workspace);
+}
+
+
+void hog(struct hog_context *self,const struct hog_image image) {
+    struct workspace* ws=(struct workspace*)self->workspace;
+    
+    // Compute gradients and convert to polar
+    conv(&ws->dx,image.type,image.buf);
+    conv(&ws->dy,image.type,image.buf);
+    GradientHistogram(&ws->gh,ws->dx.out,ws->dy.out);
+}
+
+void* hog_features_alloc(const struct hog_context *self,void* (*alloc)(size_t nbytes)) {
+    return alloc(features_nbytes(self));
+}
+
+// FIXME: require caller to give buffer size
+void hog_features_copy(const struct hog_context *self, void *buf) {
+    struct workspace *ws=(struct workspace*)self->workspace;    
+    GradientHistogramCopyLastResult(ws,buf,features_nbytes(self));
+}
+
+
+void hog_features_strides(const struct hog_context *self,struct hog_feature_dims *strides) {
+    struct hog_feature_dims shape;
+    hog_features_shape(self,&shape);
+    *strides=(struct hog_feature_dims) {
+        .x=1,
+        .y=shape.x,
+        .bin=shape.x*shape.y
+    };
+}
+
+void hog_features_shape(const struct hog_context *self,struct hog_feature_dims *shape) {
+    *shape=(struct hog_feature_dims) {
+        .x=self->w/self->params.cell.w,
+        .y=self->h/self->params.cell.h,
+        .bin=self->params.nbins
+    };
+}
